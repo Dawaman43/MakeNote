cbfd4c2b54a65fa6f744409dccf33e760a80a7a6
dawaman43 <dawitworkujima@gmail.com>
2026-02-03 00:51:00 +0300

    added first code push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div
+	class="flex h-screen w-full items-center justify-center bg-secondary-foreground text-4xl font-bold text-primary-foreground"
+>
+	<div class="flex flex-col items-center justify-center gap-4 p-4">
+		<h1 class="text-6xl">Log your notes here</h1>
+		<button class="cursor-pointer rounded-xl bg-primary-foreground p-4 text-secondary-foreground">
+			<a href="/home"> MarkNote </a>
+		</button>
+	</div>
+</div>
